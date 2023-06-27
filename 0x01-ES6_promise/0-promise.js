7d264fff1752cc9d67911d7ca04c41eabf40a0fb
kokole12 <kokoleismail886@gmail.com>
2023-06-27 20:22:27 +0300

    empty promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
-/* creating an empty promise */
+/* eslint-disabling for unused callbacks */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
+	return new Promise((resolve, reject) => {});
 }
