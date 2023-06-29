554387b0ff472e6a1898b2579ca19333bac30144
kokole12 <kokoleismail886@gmail.com>
2023-06-29 22:29:03 +0300

    displaying signs

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,18 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    this._code = val;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
