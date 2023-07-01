126951c0189079d52ce300491158e1c278a66b06
kokole12 <kokoleismail886@gmail.com>
2023-07-01 23:52:09 +0300

    async await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,6 +3,12 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   const photo = await uploadPhoto();
   const user = await createUser();
+  if (photo === null) {
+    return {
+      photo: {},
+      user: {},
+    };
+  }
 
   return {
     photo: { status: photo.status, body: photo.body },
