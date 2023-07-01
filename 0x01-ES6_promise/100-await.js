51ccdd9b3ed647d4e471bc8abe0a7de9cb718734
kokole12 <kokoleismail886@gmail.com>
2023-07-01 23:47:20 +0300

    async await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const photo = await uploadPhoto();
+  const user = await createUser();
+
+  return {
+    photo: { status: photo.status, body: photo.body },
+    user: { firstName: user.firstName, lastName: user.lastName },
+  };
+}
