1b27298257905233bfc21afb54646bae759383b2
kokole12 <kokoleismail886@gmail.com>
2023-07-04 15:45:59 +0300

    first map in javascript

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const grocerMap = new Map();
+  grocerMap.set('Apples', 10);
+  grocerMap.set('Tomatoes', 10);
+  grocerMap.set('Pasta', 1);
+  grocerMap.set('Rice', 1);
+  grocerMap.set('Banana', 5);
+
+  return grocerMap;
+}
