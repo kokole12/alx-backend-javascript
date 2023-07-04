ed3df6264557561a2393787db09cfe472d7d8f62
kokole12 <kokoleismail886@gmail.com>
2023-07-04 14:41:46 +0300

    caparing values

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+/* eslint-disable consistent-return */
+export default function hasValuesFromArray(set, array) {
+  for (const item of array) {
+    if (!set.has(item)) {
+      return false;
+    }
+    return true;
+  }
+}
