91ab2d707d2e605429c61ab6da28aa232326732a
kokole12 <kokoleismail886@gmail.com>
2023-08-22 22:05:27 +0300

    fixing eslint error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', function (input) {
-    process.stdout.write('Your name is: ' + input.toString())
-    process.exit()
-})
+process.stdin.on('data', (input) => {
+  process.stdout.write(`Your name is: ${input.toString()}`);
+  process.exit();
+});
 
-process.on('exit', function(){
-    process.stdout.write("This important software is now closing")
-})
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing');
+});
