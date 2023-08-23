903cbb107c5036612fec70ff8e2d71a782c73189
kokole12 <kokoleismail886@gmail.com>
2023-08-23 21:45:28 +0300

    tests

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,9 +6,8 @@ const assert = require('chai').assert;
 
 describe('sendPaymentRequestApi', function() {
     it('check if utils is called', function() {
-        const spy = sinon.spy(console, "log");
         const stub = sinon.stub(Utils, "calculateNumber").returns(10);
         sendPaymentRequestToApi(100, 20);
-        spy.restore();
+        stub.restore();
     });
 });
