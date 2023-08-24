13aa3b2961131574ab652959f1e51df5c313917c
kokole12 <kokoleismail886@gmail.com>
2023-08-24 12:35:34 +0300

    task5 fiing test file

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,10 +1,23 @@
-const expect = require('chai').expect;
-const sendPaymentRequestApi = require('./5-payment')
+const {describe, it} = require("mocha");
+const sinon = require("sinon");
+const sendPaymentRequestToApi = require("./5-payment");
+const expect = require("chai").expect;
 
-describe('sendPaymentRequestToAPI', function() {
-    it('testing the return from console', function() {
-        const val = sendPaymentRequestApi(100, 20);
-        console.log(val)
-        expect(val).to.equal('The total is: 20');
+describe("sendPaymentRequestToApi", function() {
+    beforeEach("Set up spy to use for each test", function() {
+	sinon.spy(console, "log");
+    });
+    afterEach("restore spy after each test", function() {
+	console.log.restore();
+    });
+    it("check that console.log is called with the right arg", function() {
+	sendPaymentRequestToApi(100, 20);
+
+	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
+    });
+    it("check that console.log is called with the right arg", function() {
+	sendPaymentRequestToApi(10, 10);
+
+	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
     });
 });
