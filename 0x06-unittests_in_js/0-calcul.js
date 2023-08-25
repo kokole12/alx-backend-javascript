fb11ab3565f2d11e00db7ad83081906f68baec56
kokole12 <kokoleismail886@gmail.com>
2023-08-25 11:52:19 +0300

    fixing task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,8 +1,10 @@
 function calculateNumber(a, b) {
-    const val1 = Math.round(a);
-    const val2 = Math.round(b);
+    const val1 = Number(a);
+    const val2 = Number(b);
 
-    return (val1 + val2);
+    if (Number.isNaN(val1) || Number.isNaN(val2))
+    throw TypeError('Parameters must be numbers');
+    return (Math.round(val1) + Math.round(val2));
 }
 
 module.exports = calculateNumber;
